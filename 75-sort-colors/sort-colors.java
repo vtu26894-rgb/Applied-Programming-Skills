41fe9dbac8b8f80b2bb90f3d15c62e153ab92623
vtu26894-rgb <vtu26894@veltech.edu.in>
2026-02-09 21:21:47 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (58.52%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,18 @@
+class Solution {
+    public static void sortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.length - 1;
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                int t = nums[low];
+                nums[low++] = nums[mid];
+                nums[mid++] = t;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                int t = nums[mid];
+                nums[mid] = nums[high];
+                nums[high--] = t;
+            }
+        }
+    }
+}
