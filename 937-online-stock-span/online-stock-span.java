8b199040356b0650b5bf1b069cf44b05b0409a96
vtu26894-rgb <vtu26894@veltech.edu.in>
2026-02-09 21:37:45 +0530

    Time: 30 ms (72.94%) | Memory: 54.4 MB (58.90%) - LeetSync

---
+++ 937-online-stock-span/online-stock-span.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+class StockSpanner {
+    private Stack<int[]> stack;
+    public StockSpanner() {
+        stack = new Stack<>();
+    }
+    public int next(int price) {
+        int span = 1;
+        while (!stack.isEmpty() && stack.peek()[0] <= price) {
+            span += stack.pop()[1];
+        }
+        stack.push(new int[]{price, span});
+        return span;
+    }
+    public static void main(String[] args) {
+        StockSpanner stockSpanner = new StockSpanner();
+        System.out.println(stockSpanner.next(100));
+        System.out.println(stockSpanner.next(80));
+        System.out.println(stockSpanner.next(60));
+        System.out.println(stockSpanner.next(70));
+        System.out.println(stockSpanner.next(60));
+        System.out.println(stockSpanner.next(75));
+        System.out.println(stockSpanner.next(85));
+    }
+}
