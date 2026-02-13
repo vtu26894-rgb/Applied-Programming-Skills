d42580082b290349fadb08ed2325ff09f1661bd7
vtu26894-rgb <vtu26894@veltech.edu.in>
2026-02-13 10:20:30 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (69.10%) - LeetSync

---
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                // Step 2: Find start of cycle
+                ListNode entry = head;
+                while (entry != slow) {
+                    entry = entry.next;
+                    slow = slow.next;
+                }
+                return entry;
+            }
+        }
+        return null;
+    }
+}
