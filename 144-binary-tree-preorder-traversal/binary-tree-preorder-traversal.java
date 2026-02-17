884804a0ace593915dd4f8636e1b157a14e89cff
vtu26894-rgb <vtu26894@veltech.edu.in>
2026-02-17 09:26:43 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (51.07%) - LeetSync

---
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+public class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        preorder(root, result);
+        return result;
+    }
+    private void preorder(TreeNode node, List<Integer> result) {
+        if (node == null) return;
+        result.add(node.val);
+        preorder(node.left, result);
+        preorder(node.right, result);
+    }
+}
