5f62421cd539d43c9d5fe14768784620e10fd54a
vtu26894-rgb <vtu26894@veltech.edu.in>
2026-04-17 10:05:26 +0530

    Time: 1 ms (99.84%) | Memory: 55.6 MB (51.66%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+
+        int candidate = 0;
+        int count = 0;
+
+        // Boyer–Moore Voting Algorithm
+        for (int num : nums) {
+
+            if (count == 0) {
+                candidate = num;
+            }
+
+            if (candidate == num) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return candidate;
+    }
+}
